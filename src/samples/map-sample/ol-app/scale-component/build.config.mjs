57346819f9ee00fb78f88c96a5c727896a023b0f
Jan Schulte <j.schulte@52north.org>
2023-03-17 10:42:24 +0100

    improve sidebar with content section

---
+++ src/samples/map-sample/ol-app/scale-component/build.config.mjs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+import { defineBuildConfig } from "@open-pioneer/build-support";
+
+export default defineBuildConfig({
+    styles: "./style.scss",
+    ui: {
+        references: ["ol-map.MapRegistry"]
+    }
+});
