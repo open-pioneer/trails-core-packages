e0b2fae8bff2d4e62a1d88eb0db537ee47ebd138
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-07-23 15:42:35 +0200

    Update dependencies (#59)

---
+++ .changeset/rude-tips-pretend.md
@@ -0,0 +1,10 @@
+---
+"@open-pioneer/authentication-keycloak": patch
+"@open-pioneer/chakra-integration": patch
+"@open-pioneer/authentication": patch
+"@open-pioneer/react-utils": patch
+"@open-pioneer/test-utils": patch
+"@open-pioneer/runtime": patch
+---
+
+Update dependencies
--- src/packages/chakra-integration/index.ts
+++ src/packages/chakra-integration/index.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 export * from "@chakra-ui/react";
-export { CustomChakraProvider } from "./Provider";
+export { type CustomChakraProviderProps, CustomChakraProvider } from "./Provider";
 export {
     FixedDrawer as Drawer,
     FixedAlertDialog as AlertDialog,
