5323957a671a97bfe04e8fdca435bceb4df84ab3
Arne Vogt <a.vogt@52north.org>
2025-01-23 16:56:08 +0100

    start sample migration to chakra v3

--- src/samples/chakra-sample/chakra-app/SampleUI.tsx
+++ src/samples/chakra-sample/chakra-app/SampleUI.tsx
@@ -1,15 +1,17 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { Toaster as ChakraToaster, CreateToasterReturn, Portal, Toast } from "@chakra-ui/react";
-import {
-    Button,
+import {    Button,
     Container,
     createToaster,
     Spinner,
     Stack,
     Text,
-    useEnvironmentContext
-} from "@open-pioneer/chakra-integration";
+    Heading,
+    Link,
+    StackSeparator,
+    Box, Toaster as ChakraToaster, CreateToasterReturn, Portal, Toast , useEnvironmentContext,
+    useDisclosure,
+    DrawerBackdrop} from "@chakra-ui/react";
 import { useMemo, useState } from "react";
 import {
     AccordionItem,
@@ -28,45 +30,91 @@ import {
     DialogTitle,
     DialogTrigger
 } from "./snippets/dialog";
+import { Tooltip } from "./snippets/tooltip";
+import { Alert } from "./snippets/alert";
+import { DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, DrawerRoot, DrawerActionTrigger, DrawerCloseTrigger } from "./snippets/drawer";
 
 export function SampleUI() {
     const [toaster, toasterUI] = useToaster();
     return (
-        <Container>
-            {toasterUI}
-            <Button onClick={(e) => console.log("Button clicked", e)}>Click me</Button>
+        <div style={{ overflow: "auto", height: "100%", width: "100%" }}>
+            <Container>
+                <Heading mb={5}>chakra technical demo</Heading>
+                <LinkComponent></LinkComponent>
+                <ComponentStack toaster={toaster}></ComponentStack>
+                {toasterUI}
+                <Button onClick={(e) => console.log("Button clicked", e)}>Click me</Button>
 
-            <Text>I am Text</Text>
+                <Text>I am Text</Text>
 
-            <AccordionDemo />
-            <Spinner />
+                <AccordionDemo />
+                <Spinner />
 
-            <DialogRoot>
-                <DialogTrigger asChild>
-                    <Button variant="outline">Open Me</Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Dialog Title</DialogTitle>
-                    </DialogHeader>
-                    <DialogBody>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                    </DialogBody>
-                    <DialogFooter>
-                        <DialogActionTrigger asChild>
-                            <Button variant="outline">Cancel</Button>
-                        </DialogActionTrigger>
-                        <Button>Save</Button>
-                    </DialogFooter>
-                    <DialogCloseTrigger />
-                </DialogContent>
-            </DialogRoot>
 
-            <ToastExample toaster={toaster}></ToastExample>
-        </Container>
+            </Container>
+        </div>
+    );
+}
+
+function LinkComponent() {
+    return (
+        <Text>
+            This is a{" "}
+            <Link href="https://chakra-ui.com" target="_blank">
+                link to Chakra&apos;s Design system
+            </Link>
+        </Text>
+    );
+}
+
+const ComponentStack = (props: { toaster: CreateToasterReturn }) =>{
+    return (
+        <Stack mb={5} mt={5} separator={<StackSeparator />} gap="24px" align="stretch">
+            <Box>
+                <PortalExample/>
+            </Box>
+            
+            <Box>
+                <TooltipExample />
+            </Box>
+            <Box>
+                <ToastExample toaster={props.toaster}/>
+            </Box>
+            <Box>
+                <AlertExample />
+            </Box>
+            <Box>
+                <DialogExample />
+            </Box>
+            <Box>
+                <DrawerExample />
+            </Box>
+            {/*<Box>
+                <PopoverExample />
+            </Box>
+            <Box>
+                <RadioGroupExample />
+            </Box> */}
+        </Stack>
+    );
+};
+
+function PortalExample() {
+    return (
+        <Box bg="background_secondary">
+            <Heading size="sm">Portal Example: </Heading>
+            This is box and displayed here. Scroll/Look down to see the portal that is added at the
+            end of document.body. The Portal is part of this Box.
+            <Portal>This is the portal content!</Portal>
+        </Box>
+    );
+}
+
+function TooltipExample() {
+    return (
+        <Tooltip showArrow content="Button Tooltip" aria-label="A tooltip" positioning={{placement: "top"}}>
+            <Button>Button with a tooltip</Button>
+        </Tooltip>
     );
 }
 
@@ -101,6 +149,72 @@ const ToastExample = (props: { toaster: CreateToasterReturn }) => {
         </Button>
     );
 };
+
+function AlertExample() {
+    return (
+        <Alert status="error" title={"Test Alert!"}>
+            This is a test alert (error)
+        </Alert>
+    );
+}
+
+function DialogExample() {
+    return (<DialogRoot>
+        <DialogTrigger asChild>
+            <Button variant="outline">Open Me</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Dialog Title</DialogTitle>
+            </DialogHeader>
+            <DialogBody>
+                <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </DialogBody>
+            <DialogFooter>
+                <DialogActionTrigger asChild>
+                    <Button variant="outline">Cancel</Button>
+                </DialogActionTrigger>
+                <Button>Save</Button>
+            </DialogFooter>
+            <DialogCloseTrigger />
+        </DialogContent>
+    </DialogRoot>);
+}
+
+function DrawerExample() {
+    const { open, onOpen, onClose } = useDisclosure();
+    return (
+        <>
+            <Button onClick={onOpen}>
+                Open Drawer
+            </Button>
+            <DrawerRoot
+                open={open}
+                placement="start"
+                onOpenChange={onClose}
+                size={"sm"}
+            >
+                <DrawerBackdrop></DrawerBackdrop>
+                <DrawerContent>
+                    <DrawerHeader>This is the drawer header</DrawerHeader>
+
+                    <DrawerBody>This is the body.</DrawerBody>
+
+                    <DrawerFooter>
+                        <DrawerActionTrigger asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DrawerActionTrigger>
+                        <Button>Save</Button>
+                    </DrawerFooter>        
+                    <DrawerCloseTrigger />
+                </DrawerContent>
+            </DrawerRoot>
+        </>
+    );
+}
 
 const items = [
     { value: "first-item", title: "First Item", text: "Some value 1..." },
