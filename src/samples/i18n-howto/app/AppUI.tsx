822b7248e2bf89a0736df4a3fdcb156e8bcd6444
Arne Vogt <a.vogt@52north.org>
2025-01-24 13:14:36 +0100

    migrate i18n examples to chakra v3

---
+++ src/samples/i18n-howto/app/AppUI.tsx
@@ -0,0 +1,220 @@
+// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
+import {
+    Container,
+    Stack,
+    Text,
+    Heading,
+    StackSeparator,
+    Box,
+    Input,
+} from "@chakra-ui/react";
+import { useIntl } from "open-pioneer:react-hooks";
+import { useState } from "react";
+import { Radio, RadioGroup } from "./snippets/radio";
+import { NumberInputField, NumberInputRoot } from "./snippets/number-input";
+
+export function AppUI() {
+    const intl = useIntl();
+    return (
+        <Container>
+            <Heading as="h1" size="lg">
+                {intl.formatMessage({ id: "heading" })}
+            </Heading>
+            <Text>{intl.formatMessage({ id: "text" })}</Text>
+            <ExampleStack></ExampleStack>
+        </Container>
+    );
+}
+
+function ExampleStack() {
+    return (
+        <Stack
+            mb={5}
+            mt={5}
+            separator={<StackSeparator borderColor="gray.200" />}
+            gap="24px"
+            align="stretch"
+        >
+            <Box bg="white" w="100%" p={4} color="black" borderWidth="1px" borderColor="black">
+                <InterpolationExample></InterpolationExample>
+            </Box>
+            <Box bg="white" w="100%" p={4} color="black" borderWidth="1px" borderColor="black">
+                <PluralsExample></PluralsExample>
+            </Box>
+            <Box bg="white" w="100%" p={4} color="black" borderWidth="1px" borderColor="black">
+                <SelectionExample></SelectionExample>
+            </Box>
+            <Box bg="white" w="100%" p={4} color="black" borderWidth="1px" borderColor="black">
+                <NumberFormatExample></NumberFormatExample>
+            </Box>
+            <Box bg="white" w="100%" p={4} color="black" borderWidth="1px" borderColor="black">
+                <DateTimeFormatExample></DateTimeFormatExample>
+            </Box>
+        </Stack>
+    );
+}
+
+function InterpolationExample() {
+    const intl = useIntl();
+    const [value, setValue] = useState("");
+    return (
+        <>
+            <Heading as="h4" size="md">
+                {intl.formatMessage({ id: "interpolation.heading" })}
+            </Heading>
+            <Input
+                value={value}
+                onChange={(evt) => setValue(evt.target.value)}
+                placeholder={intl.formatMessage({ id: "interpolation.placeholder" })}
+                size="sm"
+            />
+            <Text mb="8px">
+                {intl.formatMessage({ id: "interpolation.value" }, { name: value })}
+            </Text>
+        </>
+    );
+}
+
+function PluralsExample() {
+    const intl = useIntl();
+    const [value, setValue] = useState("1");
+    return (
+        <>
+            <Heading as="h4" size="md">
+                {intl.formatMessage({ id: "plurals.heading" })}
+            </Heading>
+            <RadioGroup onValueChange={(e) => setValue(e.value)} value={value}>
+                <Stack gap={4} direction="row">
+                    <Radio value="0">
+                        0
+                    </Radio>
+                    <Radio value="1">
+                        1
+                    </Radio>
+                    <Radio value="42">
+                        42
+                    </Radio>
+                    <Radio value="99">
+                        99
+                    </Radio>
+                </Stack>
+            </RadioGroup>
+            <Text mb="8px">{intl.formatMessage({ id: "plurals.value" }, { n: value })}</Text>
+        </>
+    );
+}
+
+function SelectionExample() {
+    const intl = useIntl();
+    const [value1, setValue1] = useState("");
+    const [value2, setValue2] = useState("male");
+    return (
+        <>
+            <Heading as="h4" size="md">
+                {intl.formatMessage({ id: "selection.heading" })}
+            </Heading>
+            <Input
+                value={value1}
+                onChange={(evt) => setValue1(evt.target.value)}
+                placeholder={intl.formatMessage({ id: "interpolation.placeholder" })}
+                size="sm"
+                mb={"5px"}
+            />
+            <RadioGroup onValueChange={(e) => setValue2(e.value)} value={value2}>
+                <Stack gap={4} direction="row">
+                    <Radio value="female">
+                        {intl.formatMessage({ id: "selection.gender.female" })}
+                    </Radio>
+                    <Radio value="male">
+                        {intl.formatMessage({ id: "selection.gender.male" })}
+                    </Radio>
+                    <Radio value="other">
+                        {intl.formatMessage({ id: "selection.gender.other" })}
+                    </Radio>
+                </Stack>
+            </RadioGroup>
+            <Text mb="8px">
+                {intl.formatMessage({ id: "selection.value" }, { name: value1, gender: value2 })}
+            </Text>
+        </>
+    );
+}
+
+function NumberFormatExample() {
+    const intl = useIntl();
+    const [value, setValue] = useState("2334232.24");
+    return (
+        <>
+            <Heading as="h4" size="md">
+                {intl.formatMessage({ id: "numberformat.heading" })}
+            </Heading>
+            <NumberInputRoot
+                onValueChange={(valueChangeDetails) => {setValue(valueChangeDetails.value);}}
+                value={value}
+                step={0.25}
+                formatOptions={{minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: false}}
+            >
+                <NumberInputField />
+            </NumberInputRoot>
+            <Text mb="8px">
+                {intl.formatMessage({ id: "numberformat.example.currency1" })}
+                {intl.formatNumber(+value, { style: "currency", currency: "EUR" })}
+            </Text>
+            <Text mb="8px">
+                {intl.formatMessage({ id: "numberformat.example.currency2" })}
+                {intl.formatNumber(+value, {
+                    style: "currency",
+                    currency: "EUR",
+                    currencyDisplay: "name"
+                })}
+            </Text>
+            <Text mb="8px">
+                {intl.formatMessage({ id: "numberformat.example.unit1" })}
+                {intl.formatNumber(+value, { style: "unit", unit: "terabyte-per-second" })}
+            </Text>
+            <Text mb="8px">
+                {intl.formatMessage({ id: "numberformat.example.unit2" })}
+                {intl.formatNumber(+value, {
+                    style: "unit",
+                    unit: "terabyte-per-second",
+                    unitDisplay: "long"
+                })}
+            </Text>
+        </>
+    );
+}
+
+function DateTimeFormatExample() {
+    const intl = useIntl();
+    const [value, setValue] = useState("2023-02-19T19:02");
+    return (
+        <>
+            <Heading as="h4" size="md">
+                {intl.formatMessage({ id: "datetimeformat.heading" })}
+            </Heading>
+            <Input
+                value={value}
+                onChange={(evt) => setValue(evt.target.value)}
+                size="md"
+                type="datetime-local"
+            />
+            <Text mb="8px">
+                {intl.formatMessage({ id: "datetimeformat.timelabel" })}
+                {intl.formatDate(value, { dateStyle: "full", timeStyle: "short" })}
+            </Text>
+            <Text mb="8px">
+                {intl.formatMessage({ id: "datetimeformat.relativetimelabel" })}
+                {intl.formatRelativeTime(getDeltaTime(value), "minute", {
+                    numeric: "auto",
+                    style: "long"
+                })}
+            </Text>
+        </>
+    );
+}
+
+function getDeltaTime(datetime: string): number {
+    const delta = new Date(datetime).getTime() - new Date().getTime();
+    return Math.round(delta / 60000);
+}
---
+++ src/samples/i18n-sample/i18n-app/I18nUI.tsx
@@ -0,0 +1,131 @@
+// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
+import { useIntl, useService } from "open-pioneer:react-hooks";
+import { ReactNode } from "react";
+import { ApplicationContext } from "@open-pioneer/runtime";
+import {
+    Center,
+    Container,
+    Text,
+    Heading,
+    List,
+    Separator,
+    Button,
+    VStack,
+    HStack
+} from "@chakra-ui/react";
+import { SamplePackageComponent } from "i18n-sample-package/SamplePackageComponent";
+
+export function I18nUI() {
+    const intl = useIntl();
+    const appCtx = useService<ApplicationContext>("runtime.ApplicationContext");
+    const locale = appCtx.getLocale();
+    const supportedLocales = appCtx.getSupportedLocales();
+    const name = "Müller";
+    const list = ["Hans", "Peter", "Hape"];
+
+    return (
+        <Container>
+            <Heading size="lg" mb={4}>
+                {intl.formatMessage({ id: "content.header" })}
+            </Heading>
+
+            <Text mb={4}>{intl.formatMessage({ id: "content.description" })}</Text>
+
+            <List.Root mb={4}>
+                <List.Item>Current locale: {locale}</List.Item>
+                <List.Item>Supported locales: {supportedLocales.join(", ")}</List.Item>
+                <List.Item>
+                    Current date and time:{" "}
+                    {intl.formatDate(new Date(), { dateStyle: "full", timeStyle: "short" })}
+                </List.Item>
+                <List.Item>
+                    Relative Time - 1:{" "}
+                    {intl.formatRelativeTime(1, "minute", { numeric: "auto", style: "long" })}
+                </List.Item>
+                <List.Item>
+                    Relative Time - 15:{" "}
+                    {intl.formatRelativeTime(15, "minute", { numeric: "auto", style: "long" })}
+                </List.Item>
+                <List.Item>
+                    Relative Time - 0:{" "}
+                    {intl.formatRelativeTime(0, "minute", { numeric: "auto", style: "long" })}
+                </List.Item>
+                <List.Item>
+                    Large number (Currency):{" "}
+                    {intl.formatNumber(1234567.89, { style: "currency", currency: "EUR" })}
+                </List.Item>
+                <List.Item>
+                    Large number (Unit):{" "}
+                    {intl.formatNumber(1234567.89, { style: "unit", unit: "kilogram-per-second" })}
+                </List.Item>
+                <List.Item>
+                    Plural - Count 0: {intl.formatMessage({ id: "content.testplural" }, { n: 0 })}
+                </List.Item>
+                <List.Item>
+                    Plural - Count 1: {intl.formatMessage({ id: "content.testplural" }, { n: 1 })}
+                </List.Item>
+                <List.Item>
+                    Plural - Count 2: {intl.formatMessage({ id: "content.testplural" }, { n: 2 })}
+                </List.Item>
+                <List.Item>List: {intl.formatList(list, { type: "conjunction" })}</List.Item>
+                <List.Item>
+                    Gender - female:{" "}
+                    {intl.formatMessage(
+                        { id: "content.testgender" },
+                        { gender: "female", name: name }
+                    )}
+                </List.Item>
+                <List.Item>
+                    Gender - male:{" "}
+                    {intl.formatMessage(
+                        { id: "content.testgender" },
+                        { gender: "male", name: name }
+                    )}
+                </List.Item>
+                <List.Item>
+                    Gender - other:{" "}
+                    {intl.formatMessage(
+                        { id: "content.testgender" },
+                        { gender: "other", name: name }
+                    )}
+                </List.Item>
+            </List.Root>
+
+            <Center mb={4}>
+                <LocalePicker />
+            </Center>
+
+            <Separator my={4} />
+
+            <Text mb={4}>
+                This component is from another package which does <em>not</em> support de-simple by
+                itself:
+            </Text>
+            <SamplePackageComponent />
+        </Container>
+    );
+}
+
+function LocalePicker() {
+    const appCtx = useService<ApplicationContext>("runtime.ApplicationContext");
+    const intl = useIntl();
+    const locales = appCtx.getSupportedLocales();
+
+    // One entry for every supported locale (to force it) and one empty
+    // to pick the default behavior.
+    const makeButton = (locale: string | undefined) => (
+        <Button key={locale ?? ""} onClick={() => appCtx.setLocale(locale)}>
+            {locale ?? intl.formatMessage({ id: "picker.default" })}
+        </Button>
+    );
+    const buttons: ReactNode[] = locales.map((locale) => makeButton(locale));
+    buttons.unshift(makeButton(undefined));
+
+    return (
+        <VStack>
+            <Text>{intl.formatMessage({ id: "picker.choose" })}</Text>
+            <HStack gap={2}>{buttons}</HStack>
+        </VStack>
+    );
+}
