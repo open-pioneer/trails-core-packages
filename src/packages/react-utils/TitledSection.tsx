50d4b81dfd8ce9ca4a1e04f547ee5515786fedef
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-02-20 15:43:03 +0100

    Update license headers

--- src/packages/react-utils/TitledSection.tsx
+++ src/packages/react-utils/TitledSection.tsx
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { Heading, HeadingProps } from "@open-pioneer/chakra-integration";
 import { FC, ForwardedRef, ReactNode, createContext, forwardRef, useContext } from "react";
