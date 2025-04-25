ef79682f933ebe982d9fecc02cc7d9be277d207f
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-04-25 12:53:23 +0200

    Remove unused import

--- src/packages/react-utils/TitledSection.test.tsx
+++ src/packages/react-utils/TitledSection.test.tsx
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { render } from "@testing-library/react";
-import { expect, it, vi } from "vitest";
+import { expect, it } from "vitest";
 import { ConfigureTitledSection, SectionHeading, TitledSection } from "./TitledSection";
 import { ReactNode, useEffect, useRef } from "react";
 import { PackageContextProvider } from "@open-pioneer/test-utils/react";
