88b56de3b8dfd59f4ac247e82a2dd6859b58250b
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-07-23 16:24:19 +0200

    Remove obsolete note

--- src/packages/authentication-keycloak/build.config.mjs
+++ src/packages/authentication-keycloak/build.config.mjs
@@ -10,8 +10,6 @@ export default defineBuildConfig({
             provides: [
                 // The generic interface, used by the AuthService
                 "authentication.AuthPlugin",
-
-                // Concrete interface for this plugin (note: no additional API yet).
                 "authentication-keycloak.KeycloakAuthPlugin"
             ],
             references: {
