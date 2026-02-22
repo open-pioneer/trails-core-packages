4953aa50a52e48e172b009780068693543569c44
Marko Reiprecht <m.reiprecht@conterra.de>
2026-02-22 19:10:43 +0100

    ensure that the 'started' state contract is not broken by initialization the api independently

--- src/packages/runtime/app/AppInstance.ts
+++ src/packages/runtime/app/AppInstance.ts
@@ -170,7 +170,7 @@ export class AppInstance {
         );
 
         // init api, but do not wait for it
-        this.initAPI(serviceLayer);
+        const apiPromise = this.initAPI(serviceLayer);
         this.checkAbort();
 
         // Launch react
@@ -187,6 +187,12 @@ export class AppInstance {
         });
         const component = this.options.elementOptions.component ?? EmptyComponent;
         this.reactIntegration.render(createElement(component));
+
+        // wait for api, to ensure that the "started" state is only reached after the API is available.
+        // but do not block the react rendering, to allow for a fast Time to Interactive and to show potential errors in the UI even if the API fails.
+        await apiPromise;
+        this.checkAbort();
+
         this.state = "started";
 
         this.triggerApplicationLifecycleEvent("after-start");
@@ -284,6 +290,7 @@ export class AppInstance {
                 this.apiPromise.promise.catch(() => {});
             }
             this.apiPromise.reject(ex);
+            throw ex;
         }
     }
 
