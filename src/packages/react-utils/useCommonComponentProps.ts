b81bf73d80afac8336d7475f94fef9570f53a3e0
a.marks <a.marks@conterra.de>
2026-05-19 09:34:53 +0200

    remove classnames dep for own implementation

--- src/packages/react-utils/useCommonComponentProps.ts
+++ src/packages/react-utils/useCommonComponentProps.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { SystemStyleObject } from "@chakra-ui/react";
-import classNames from "classnames";
+import { classNames } from "./classNames";
 import { HTMLAttributes, AriaAttributes, useMemo, AriaRole } from "react";
 
 /**
